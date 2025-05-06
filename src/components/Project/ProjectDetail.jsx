249f037aafc86722280fb76a1323440440a6d12b
Elise Hrabik <elise-hrabik@student.kirkwood.edu>
2025-05-06 14:08:18 -0500

    Improved Project Details Pages

--- src/components/Project/ProjectDetail.jsx
+++ src/components/Project/ProjectDetail.jsx
@@ -13,20 +13,44 @@ function ProjectDetail() {
     <div className="project-detail">
       <h1 className="project-detail__title">{project.title}</h1>
       <p className="project-detail__text">{project.description}</p>
-      <a
-        href={project.websiteUrl}
-        className="project-detail__link"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        See Website
-      </a>
-      <img
-        src={project.imageUrl}
-        alt={project.title}
-        className="project-detail__image"
-      />
-      <Link to="/" className="project-detail__link">
+
+      <div className="project-detail__columns-wrapper">
+        <div className="project-detail__img-column">
+          <a
+            href={project.websiteUrl}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={project.imageUrl}
+              alt={project.title}
+              className="project-detail__image"
+            />
+          </a>
+        </div>
+
+        <div className="project-detail__skills-column">
+          <h2 className="project-detail__skills-title">Technologies Used</h2>
+          <ul className="project-detail__skills-list">
+            {project.technologies.map((tech, index) => (
+              <li key={index} className="project-detail__skills-item">
+                {project.technologies[index]}
+              </li>
+            ))}
+          </ul>
+          <h2 className="project-detail__skills-title">Skills Employed</h2>
+          <ul className="project-detail__skills-list">
+            {project.skills.map((skills, index) => (
+              <li key={index} className="project-detail__skills-item">
+                {project.skills[index]}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <p className="project-detail__full-description">{project.full_description}</p>
+      <Link to="/#projects" className="project-detail__link">
         Back to Projects
       </Link>
     </div>
