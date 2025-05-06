422c789cf959a37fca7266d781ecbba772860048
Elise Hrabik <elise-hrabik@student.kirkwood.edu>
2025-05-06 12:21:04 -0500

    Updated Project Images

--- src/components/Project/Projects.jsx
+++ src/components/Project/Projects.jsx
@@ -1,29 +1,33 @@
-import ProjectCard from "./ProjectCard";
-import projectList from "./projectList";
+import ProjectCard from './ProjectCard';
+import projectList from './projectList';
 
 function Projects() {
-    return (
-        <div className="projects">
-            <h1 className="projects__title">My Work</h1>
-            <div className="projects__grid">
-                {projectList.map((project, index) => (
-                    <div className={`projects__row ${index % 2 !== 0 ? "reverse-on-desktop" : ""}`} key={project.id}>
-                    <img
-                        src={project.imageUrl}
-                        alt={`${project.title} visual`}
-                        className="projects__image"
-                    />
-                    <ProjectCard
-                        title={project.title}
-                        description={project.description}
-                        link={project.link}
-                    />
-                </div>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className="projects">
+      <h1 className="projects__title">My Work</h1>
+      <div className="projects__grid">
+        {projectList.map((project, index) => (
+          <div
+            className={`projects__row ${
+              index % 2 !== 0 ? 'reverse-on-desktop' : ''
+            }`}
+            key={project.id}
+          >
+            <img
+              src={project.imageUrl}
+              alt={`${project.title} visual`}
+              className={`projects__image projects__image--${index + 1}`}
+            />
+            <ProjectCard
+              title={project.title}
+              description={project.description}
+              link={project.link}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
-
 
 export default Projects;
--- src/components/Project/projectList.js
+++ src/components/Project/projectList.js
@@ -1,7 +1,7 @@
 const projectList = [
-    { id: 1, title: "Emerald Park", description: "A robust Java website for a fictional park, including user authentication, filtering, and an interactive trail map.", link: "/project/1", imageUrl: '/my-portfolio/img/emeraldpark.PNG', websiteUrl: 'https://emerald-park.azurewebsites.net/' },
-    { id: 2, title: "Optimist Club", description: "A functional website for a local youth organization using Wordpress / Elementor.", link: "/project/2", imageUrl: '/my-portfolio/img/optimistclub.PNG', websiteUrl: 'https://optimistnl.org/' },
-    { id: 3, title: "Fund Fairy", description: "A collaborative Laravel PHP project for a fictional crowd funding platform with users and donations.", link: "/project/3", imageUrl: '/my-portfolio/img/fundfairy.PNG', websiteUrl: 'https://fundfairy.live'}
+    { id: 1, title: "Emerald Park", description: "A robust Java website for a fictional park, including user authentication, filtering, and an interactive trail map.", link: "/project/1", imageUrl: '/my-portfolio/img/emeraldpark.png', websiteUrl: 'https://emerald-park.azurewebsites.net/' },
+    { id: 2, title: "Fund Fairy", description: "A collaborative Laravel PHP project for a fictional crowd funding platform with users and donations.", link: "/project/3", imageUrl: '/my-portfolio/img/fundfairy.png', websiteUrl: 'https://fundfairy.live'},
+    { id: 3, title: "Optimist Club", description: "A functional website for a local youth organization using Wordpress / Elementor.", link: "/project/2", imageUrl: '/my-portfolio/img/optimist.png', websiteUrl: 'https://optimistnl.org/' },
 ];
 
 export default projectList;
